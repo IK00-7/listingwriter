a95316d15064cc0838f7a0f9eeb433965ccdcec3
IK00-7 <ismaelkhan7777@gmail.com>
2026-02-21 21:37:14 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,15 +4,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Inline SVG Favicon - No file upload needed! */}
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' fill='%2310b981' rx='15'/><text x='50' y='75' font-size='70' text-anchor='middle'>⚡</text></svg>" />
+        
         {/* Mobile Meta Tags */}
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#10b981" />
         
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
-        
-        {/* Fonts - Optional */}
+        {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
