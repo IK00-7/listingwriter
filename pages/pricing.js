0125dc21e9a2a0bce4c51288fb5ca11932254d7a
IK00-7 <ismaelkhan7777@gmail.com>
2026-02-27 18:52:55 +0000

    Update pricing.js

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
