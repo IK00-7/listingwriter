82cee606ad4af8023b518f179bba18fef910f45e
IK00-7 <ismaelkhan7777@gmail.com>
2026-03-02 21:32:29 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import { Analytics } from '@vercel/analytics/next'
